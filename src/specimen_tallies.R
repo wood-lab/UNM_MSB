ea4c6f9fc46cc330db81f217041f39dee7404e5d
Gabby Commisso <gcom@uw.edu.org>
2025-07-16 10:16:12 -0700

    more replacements: MSB:Fish:1080 and MSB:Fish:340

--- src/specimen_tallies.R
+++ src/specimen_tallies.R
@@ -558,6 +558,14 @@ impact_1931to1940<-gam_aff[ sample( which( gam_aff$combo == "impact_1931-1940"),
 # Replace with MSB:Fish:1105
 
 # 7/16/25
-# Replace GAMAFF MSB:Fish:4440 (all fish are below size limit)
+# Replace GAMAFF MSB:Fish:4440 (all fish are below new latitude size limit)
 impact_1971to1980<-gam_aff[ sample( which( gam_aff$combo == "impact_1971-1980"), 9, replace = F), ]
-# No replacement we aren't already dissecting :-(
+# No replacement we aren't already dissecting :-( So we will go ahead and dissect these and make notes in the datasheet that it doesn't have any size selection
+
+# Replace GAMAFF MSB:Fish:1080 (only 10 fish in jar)
+impact_1931to1940 <- gam_aff[ sample( which( gam_aff$combo == "impact_1931-1940"), 10, replace = F), ]
+# Replace with MSB:Fish:1101 OR 1104 
+
+# Replace GAMAFF MSB:Fish:340 (only 10 fish in jar)
+impact_1941to1950 <- gam_aff[ sample( which( gam_aff$combo == "impact_1941-1950"), 8, replace = F), ]
+# No replacements! Will not dissect
