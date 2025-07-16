05d1db105cb75cd2bdd81006c41c88d5b3913042
Bradyn Perron <bmp25@uw.edu>
2025-07-16 14:31:40 -0700

    Added Mid Rio Grande West levee parameters

--- reu/bradyn/gamaff_levee.R
+++ reu/bradyn/gamaff_levee.R
@@ -7,18 +7,19 @@ library(ggplot2)
 HYBAMA_data<- read.csv("C:/Users/Bradyn/OneDrive/GEO366/ABQ_DATA/IND_PROJ_BRADYN/data/processed/Hybognathus_amarus_processed_human_readable_2025.07.06.csv")
 levee_data<- read.csv("C:/Users/Bradyn/OneDrive/GEO366/al_midrio_R_data.csv")
 view(HYBAMA_data)
-
+#Setting Angostura and Cochiti dam bounds
 #setting Corrales Levee bounds
 
 ab_corrales_levee <- HYBAMA_data %>% 
-  mutate(individual_levee_location = case_when(
-    Latitude > 35.28136319711 ~"levee_above",
+  mutate(corrales_locale = case_when(
+    Latitude > 35.28136319711 ~"corrales_levee_above",
     Latitude >= 35.1609175651113 & Latitude <= 35.28136319711 ~ "corrales_levee",
-    Latitude < 35.1609175651113 ~ "levee_below",
+    Latitude < 35.1609175651113 ~ "corrales_levee_below",
     TRUE ~ "no_intervention"
   ))
 
 view(ab_corrales_levee)
+
 #setting Corrales levee construction date
 before_ab_corrales_levee <- ab_corrales_levee %>% 
   mutate(before_after_corrales = case_when(
@@ -27,6 +28,29 @@ before_ab_corrales_levee <- ab_corrales_levee %>%
     YearCollected < 1986 ~"before_corrales",
     TRUE ~"no_intervention"
   ))
+
 view(before_ab_corrales_levee)
+
+#setting Alb. Middle Rio Grande West Levee bounds
+ab_amrg_wlevee <- before_ab_corrales_levee %>% 
+  mutate(w_amrg_locale = case_when(
+    Latitude>35.095806160112076 ~"amrg_wlevee_above",
+    Latitude>= 34.94913162611374 & Latitude <=35.095806160112076 ~ "amrg_wlevee",
+    Latitude < 34.94913162611374 ~"amrg_wlevee_below",
+    TRUE ~ "no_intervention"
+  ))
+view(ab_amrg_wlevee)  
+#Setting Alb. Middle Rio Grande West Levee construction dates
+before_ab_amrg_wlevee<-ab_amrg_wlevee %>% 
+  mutate(before_after_wamrg=case_when(
+    YearCollected>=1956~"after_w_mrg",
+    YearCollected>=1951 & YearCollected <1956 ~"during_w_mrg",
+    YearCollected<1951 ~"before_w_mrg",
+    TRUE ~ "no_intervention"
+  ))
+view(before_ab_amrg_wlevee)  
+#renaming data
+BACI_levee<-before_ab_amrg_wlevee
+view(BACI_levee)  
 
 
