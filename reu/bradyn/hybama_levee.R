83daacd309bf8a6048015c6690539e4304b50226
Bradyn Perron <bmp25@uw.edu>
2025-07-17 22:58:51 -0700

    Begin pre-lim data analysis on Corrales and West Middle Rio Grande Levees

--- reu/bradyn/hybama_levee.R
+++ reu/bradyn/hybama_levee.R
@@ -11,34 +11,36 @@ levee_data<- read.csv("C:/Users/Bradyn/OneDrive/GEO366/al_midrio_R_data.csv")
 
 ab_corrales_levee <- HYBAMA_data %>% 
   mutate(corrales_locale = case_when(
-    Latitude > 35.28136319711 ~"corrales_levee_above",
-    Latitude >= 35.1609175651113 & Latitude <= 35.28136319711 ~ "corrales_levee",
-    Latitude < 35.1609175651113 ~ "corrales_levee_below",
+    Latitude > 35.28136319711 ~"above",
+    Latitude >= 35.1609175651113 & Latitude <= 35.28136319711 ~ "within",
+    Latitude < 35.1609175651113 ~ "below",
     TRUE ~ "no_intervention"
   ))
+#Dam system Locations and Dates
+
 #setting Corrales levee construction date
 before_ab_corrales_levee <- ab_corrales_levee %>% 
   mutate(before_after_corrales = case_when(
-    YearCollected >=1997 ~ "after_corrales",
-    YearCollected >=1986 & YearCollected<1997 ~"during_corrales",
-    YearCollected < 1986 ~"before_corrales",
+    YearCollected >=1997 ~ "after",
+    YearCollected >=1986 & YearCollected<1997 ~"during",
+    YearCollected < 1986 ~"before",
     TRUE ~"no_intervention"
   ))
 
 #setting Alb. Middle Rio Grande West Levee bounds
 ab_amrg_wlevee <- before_ab_corrales_levee %>% 
   mutate(w_amrg_locale = case_when(
-    Latitude>35.095806160112076 ~"amrg_wlevee_above",
-    Latitude>= 34.94913162611374 & Latitude <=35.095806160112076 ~ "amrg_wlevee",
-    Latitude < 34.94913162611374 ~"amrg_wlevee_below",
+    Latitude>35.095806160112076 ~"above",
+    Latitude>= 34.94913162611374 & Latitude <=35.095806160112076 ~ "within",
+    Latitude < 34.94913162611374 ~"below",
     TRUE ~ "no_intervention"
   ))
 #Setting Alb. Middle Rio Grande West Levee construction dates
 before_ab_amrg_wlevee<-ab_amrg_wlevee %>% 
   mutate(before_after_wamrg=case_when(
-    YearCollected>=1956~"after_w_mrg",
-    YearCollected>=1951 & YearCollected <1956 ~"during_w_mrg",
-    YearCollected<1951 ~"before_w_mrg",
+    YearCollected>=1956~"after",
+    YearCollected>=1951 & YearCollected <1956 ~"during",
+    YearCollected<1951 ~"before",
     TRUE ~ "no_intervention"
   ))
 #renaming data
@@ -47,60 +49,95 @@ BACI_levee<-before_ab_amrg_wlevee
 BACI_levee$parasite_sum <- rowSums(BACI_levee[, c("cope.lern", "cope.imler","mono.dact","mono.gyro","myxo.b","nem.cl","nem.unk","trem.b","trem.d","trem.diplo","trem.dlum","trem.em","trem.fim","trem.gold","trem.l","trem.meta.unk","trem.ridge")], na.rm=TRUE)
 
 #Filter for above Corrales
-above_corrales_levee<-BACI_levee[tolower(BACI_levee$corrales_locale)=="corrales_levee_above",]
+above_corrales_levee<-BACI_levee[tolower(BACI_levee$corrales_locale)=="above",]
 
 #filter for above and before corrales
-above_corrales_blevee<-above_corrales_levee[tolower(above_corrales_levee$before_after_corrales)=="before_corrales",]
-view(above_corrales_blevee)
+above_corrales_blevee<-above_corrales_levee[tolower(above_corrales_levee$before_after_corrales)=="before",]
+
 
 #filter for above and after corrales
-above_corrales_alevee<-above_corrales_levee[tolower(above_corrales_levee$before_after_corrales)=="after_corrales",]
+above_corrales_alevee<-above_corrales_levee[tolower(above_corrales_levee$before_after_corrales)=="after",]
 
 #plots for Above corrales
 plot(above_corrales_levee$parasite_sum~above_corrales_levee$YearCollected)
 summary(lm(above_corrales_levee$parasite_sum~above_corrales_levee$YearCollected))
+plot(above_corrales_levee$parasite_sum~above_corrales_levee$YearCollected,
+     main = "Number of Parasites Above Corrales Levee Over time",
+     xlab = "Year Collected",
+     ylab = "Number of Parasites",
+     pch = 19,
+     col = "purple")
+regression_above <-lm(parasite_sum ~ YearCollected,data=above_corrales_levee )
+abline(regression, col="red", lwd=2)
 
+summary(regression_above)
 
 #filter for below and in corrales
 
-below_corrales_levee<-BACI_levee[tolower(BACI_levee$corrales_locale)=="corrales_levee_below",]
+below_corrales_levee<-BACI_levee[
+  tolower(BACI_levee$corrales_locale)%in%c("below","within"),
+  ]
+#must be above west middle rio grande
+true_below_corrales<-below_corrales_levee[
+  tolower(below_corrales_levee$w_amrg_locale)=="above",
+]
+view(true_below_corrales)
 
-view(below_corrales_levee)
+#plotting below and in corrales
+plot(true_below_corrales$parasite_sum~true_below_corrales$YearCollected,
+     main = "Number of Parasites Within and Below Corrales Levee Over time",
+     xlab = "Year Collected",
+     ylab = "Number of Parasites",
+     pch = 19,
+     col = "red")
+regression <-lm(parasite_sum ~ YearCollected,data=true_below_corrales )
+abline(regression, col="blue", lwd=2)
+summary(regression)
 
-#filter for below corrales and above west middle rio grande
-true_below_corrales<-below_corrales_levee[tolower(below_corrales_levee$w_amrg_locale)=="amrg_wlevee_above",]
+summary(lm(below_corrales_levee$parasite_sum~below_corrales_levee$YearCollected))
+
+
+
 
 #Filter for above west middle rio grande
 
-above_wamrg_levees<-BACI_levee[tolower(BACI_levee$w_amrg_locale)=="amrg_wlevee_above",]
+above_wamrg_levees<-BACI_levee[
+  tolower(BACI_levee$w_amrg_locale)== "above",
+  ]
 
+true_above_wamrg<-above_wamrg_levees[
+  tolower(above_wamrg_levees$corrales_locale)=="below",
+]
+view(true_above_wamrg)
+plot(true_above_wamrg$parasite_sum ~ true_above_wamrg$YearCollected)
+       
 #filter for above and before west middle rio grande
-above_wamrg_blevees<-above_wamrg_levees[tolower(above_wamrg_levees$before_after_wamrg)=="before_w_mrg",]
+true_above_bwamrg<-true_above_wamrg[
+  tolower(true_above_wamrg$before_after_wamrg)=="before",
+  ]
+plot(true_above_bwamrg$parasite_sum ~ true_above_bwamrg$YearCollected)
+view(true_above_bwamrg)
 
 #filter for above and after west middle rio grande
-above_wamrg_alevees<-above_wamrg_levees[tolower(above_wamrg_levees$before_after_wamrg)=="after_w_mrg",]
+true_above_awamrg<-true_above_wamrg[
+  tolower(true_above_wamrg$before_after_wamrg)=="after",
+  ]
+plot(true_above_awamrg$parasite_sum~true_above_awamrg$YearCollected)
+
 view(above_wamrg_alevees)
 #filter for in and below west middle rio grande
-
-view(filtered_wamrg_alevees)
-view(filtered_wamrg_balevees)
-
-
-plot(BACI_levee$parasite_sum~BACI_levee$YearCollected)
-view(BACI_levee$corrales_locale)
-
-character_counts<-BACI_levee %>% count(before_after_wamrg)
-
-year_counts<-BACI_levee %>% count(YearCollected)
-
-print(year_counts)
-
-print(character_counts)
-
-view(character_counts)  
-view(year_counts)
-
-hist(character_counts$n)
-plot(character_counts$n~year_counts$YearCollected)
+below_wamrg_levee<-BACI_levee[
+  tolower(BACI_levee$w_amrg_locale)%in%c("below","within"),
+]
+#Plotting
+plot(below_wamrg_levee$parasite_sum ~ below_wamrg_levee$YearCollected,
+     main = "Parasite Abundace Within and Below WAMRG",
+     xlab = "Year Collected",
+     ylab = "Parasite Count",
+     pch = 19,
+     col = "orange")
+wamrg_regression <-lm(parasite_sum ~ YearCollected,data=below_wamrg_levee)
+abline(wamrg_regression, col="blue", lwd=2)
+summary(wamrg_regression)
 
 
