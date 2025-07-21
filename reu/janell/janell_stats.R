542b54047678be8335cd0150e17ff1cba8816285
janellng <janellngoldsmith@gmail.com>
2025-07-21 15:16:01 -0600

    Stats test

--- reu/janell/janell_stats.R
+++ reu/janell/janell_stats.R
@@ -14,12 +14,17 @@ library(ggeffects)
 library(reshape2)
 library(MASS)
 
+install.packages("lme4")
+install.packages("car")
+install.packages("ggeffects")
+install.packages("reshape2")
+install.packages("MASS")
 
 # R is really useful for doing quick tallies and plots.
 # Let's do some together!
 
 hyb_ama_data<-read.csv("data/processed/Hybognathus_amarus_processed_machine_readable_2025.07.06.csv")
-
+view(hyb_ama_data)
 
 # Let's do some quick data tallies - we can see how many values we have for estimates of each parasite species in each
 # host across the different treatment-decade combinations
@@ -27,7 +32,9 @@ hyb_ama_data<-read.csv("data/processed/Hybognathus_amarus_processed_machine_read
 tally_up <- hyb_ama_data %>%
   group_by(combo) %>%
   summarize(n = n())
-tally_up
+tally_up()
+
+hyb_ama_data %>% group_by(combo) %>% summarise(n = n())
 
 
 # First, we need to limit our dataset to just the diplostomatids. Let's see which parasite categories exist, and which
@@ -40,8 +47,8 @@ levels(as.factor(hyb_ama_data$psite_spp))
 
 diplo_data_separate <- hyb_ama_data %>%
   filter(psite_spp == "trem.diplo" | psite_spp == "trem.dlum" | psite_spp == "trem.em")
-
 levels(as.factor(diplo_data_separate$psite_spp))
+view(diplo_data_separate)
 
 
 # Now we want to sum up all three categories to get a total count of all diplostomatids (trem.diplo + trem.dlum +
@@ -50,15 +57,17 @@ levels(as.factor(diplo_data_separate$psite_spp))
 diplo_data <- diplo_data_separate %>%
   group_by(IndividualFishID, CatalogNumber, YearCollected, DissectionDate, TotalLength_mm, CI, combo, Latitude, Longitude) %>%
   summarize(diplo = sum(psite_count))
-diplo_data
+view(diplo_data)
 
 
 # Cool. Now we just need to merge in the bird data.
 
 bird_data<-read.csv("reu/janell/bird_data_cleaned.csv")
+view(bird_data)
 
 diplo_plus_birds <- merge(diplo_data, bird_data, by.x = "YearCollected", by.y = "Year", all.x = TRUE)
 head(diplo_plus_birds)
+view(diplo_plus_birds)
 
 
 ### PRELIMINARY ANALYSIS - CHELSEA, 21 JULY 2025
