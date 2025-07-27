6f5c44121c097a79f60dd04d4e7ac88361780e13
Chelsea Wood <chelsealwood@gmail.com>
2025-07-27 15:26:58 -0600

    updating analysis and figures

--- reu/janell/janell_stats.R
+++ reu/janell/janell_stats.R
@@ -4,7 +4,16 @@
 # Started 20 July 2025
 
 
-# Load up the packagaes you need
+# Install the packagaes you need
+
+install.packages("lme4")
+install.packages("car")
+install.packages("ggeffects")
+install.packages("reshape2")
+install.packages("MASS")
+
+
+# Load the packagaes you need
 
 library(ggplot2)
 library(tidyverse)
@@ -14,11 +23,6 @@ library(ggeffects)
 library(reshape2)
 library(MASS)
 
-install.packages("lme4")
-install.packages("car")
-install.packages("ggeffects")
-install.packages("reshape2")
-install.packages("MASS")
 
 # R is really useful for doing quick tallies and plots.
 # Let's do some together!
@@ -32,15 +36,13 @@ View(gam_aff_data)
 # Let's do some quick data tallies - we can see how many values we have for estimates of each parasite species in each
 # host across the different treatment-decade combinations
 
-tally_up <- hyb_ama_data %>%
+hyb_ama_data %>% 
   group_by(combo) %>% 
   summarize(n = n())
-tally_up
 
-hyb_ama_data %>% group_by(combo) %>% summarize(n = n())
-
-gam_aff_data %>% group_by(combo) %>% summarize(n = n())
-
+gam_aff_data %>% 
+  group_by(combo) %>% 
+  summarize(n = n())
 
 
 # First, we need to limit our dataset to just the diplostomatids. Let's see which parasite categories exist, and which
@@ -50,21 +52,31 @@ levels(as.factor(hyb_ama_data$psite_spp))
 levels(as.factor(gam_aff_data$psite_spp))
 
 
-# Looks like we want trem.diplo, trem.dlum, and trem.em. Let's trim the dataset so that it includes only those taxa.
+# Looks like, for HYBAMA, we want trem.diplo, trem.dlum, and trem.em. 
+# Let's trim the dataset so that it includes only those taxa.
 
-diplo_data_separate <- hyb_ama_data %>%
+diplo_data_hybama <- hyb_ama_data %>%
   filter(psite_spp == "trem.diplo" | psite_spp == "trem.dlum" | psite_spp == "trem.em")
-levels(as.factor(diplo_data_separate$psite_spp))
-view(diplo_data_separate)
+levels(as.factor(diplo_data_hybama$psite_spp))
+view(diplo_data_hybama)
+
+
+# Looks like, for GAMAFF, we want trem.dips. 
+# Let's trim the dataset so that it includes only that taxon.
+
+diplo_data_gamaff <- gam_aff_data %>%
+  filter(psite_spp == "trem.dips")
+levels(as.factor(diplo_data_gamaff$psite_spp))
+view(diplo_data_gamaff)
 
 
 # Now we want to sum up all three categories to get a total count of all diplostomatids (trem.diplo + trem.dlum +
 # trem.em). Let's do that with a pipe:
 
-diplo_data <- diplo_data_separate %>%
-  group_by(IndividualFishID, CatalogNumber, YearCollected, DissectionDate, TotalLength_mm, CI, combo, Latitude, Longitude) %>%
-  summarize(diplo = sum(psite_count))
-view(diplo_data)
+#diplo_data <- diplo_data_separate %>%
+ # group_by(IndividualFishID, CatalogNumber, YearCollected, DissectionDate, TotalLength_mm, CI, combo, Latitude, Longitude) %>%
+#  summarize(diplo = sum(psite_count))
+#view(diplo_data)
 
 
 # Cool. Now we just need to merge in the bird data.
@@ -72,23 +84,95 @@ view(diplo_data)
 bird_data<-read.csv("reu/janell/bird_data_cleaned.csv")
 view(bird_data)
 
-diplo_plus_birds <- merge(diplo_data, bird_data, by.x = "YearCollected", by.y = "Year", all.x = TRUE)
-head(diplo_plus_birds)
-view(diplo_plus_birds)
+hybama_plus_birds <- merge(diplo_data_hybama, bird_data, by.x = "YearCollected", by.y = "Year", all.x = TRUE)
+head(hybama_plus_birds)
+hybama_plus_birds$fish_spp<-"Hybognathus amarus"
 
-bird_data <- merge(diplo_data, bird_data, by.x = "YearCollected", by.y = "Year", all.x = TRUE)
+gamaff_plus_birds <- merge(diplo_data_gamaff, bird_data, by.x = "YearCollected", by.y = "Year", all.x = TRUE)
+head(gamaff_plus_birds)
+gamaff_plus_birds$fish_spp<-"Gambusia affinis"
+
+all_data <- rbind(hybama_plus_birds,gamaff_plus_birds)
+view(all_data)
 
 
-### PRELIMINARY ANALYSIS - CHELSEA, 21 JULY 2025
+### PRELIMINARY ANALYSIS - CHELSEA, updated 27 JULY 2025
 
-plot(diplo_plus_birds$diplo~diplo_plus_birds$Sum.of.Number.Party.Hours)
+plot(all_data$psite_count~all_data$Sum.of.Number.Party.Hours)
 
-model_1<-glm.nb(diplo~Sum.of.Number.Party.Hours+
-                    offset(log(TotalLength_mm)),data=diplo_plus_birds)
+model_1<-glmer.nb(psite_count~Sum.of.Number.Party.Hours+
+                    offset(log(TotalLength_mm))+(1|fish_spp/psite_spp),data=all_data)
 summary(model_1)
 
-plot(diplo_plus_birds$diplo~diplo_plus_birds$Sum.of.Number.Party.Hours)
+plot(all_data$psite_count~all_data$Sum.of.Number.Party.Hours)
+plot(all_data$psite_count~all_data$YearCollected)
+plot(all_data$Sum.of.Number.Party.Hours~all_data$YearCollected)
 
-plot(diplo_plus_birds$diplo~diplo_plus_birds$YearCollected)
-plot(diplo_plus_birds$Sum.of.Number.Party.Hours~diplo_plus_birds$YearCollected)
+
+### FANCIER PLOTS
+
+
+# Addressing Question 1: 
+
+q1_plot<-ggplot(all_data,aes(YearCollected,Sum.of.Number.Party.Hours))+
+  geom_point(size=4,pch=19)+
+  xlab("year collected")+
+  ylab("bird abundance (number of birds per party-hour)")+
+  theme_minimal()+
+  theme(plot.title=element_text(size=18,hjust=0.5,face="plain"),
+        axis.text.y=element_text(size=14),
+        axis.title.y=element_text(size=16),
+        axis.title.x=element_text(size=16),
+        panel.background=element_rect(fill="white",color="black"),panel.grid.major=element_line(color=NA),
+        panel.grid.minor=element_line(color=NA),plot.margin=unit(c(0,0,0,0),"cm"))+
+  #annotate("text",label="effect of year:\np < 0.0001",x = 1.9, y = 0.45, size = 6)+
+  theme(legend.position="top",legend.title = element_text(size = 18),
+        legend.text = element_text(size=14))
+q1_plot
+
+
+# Addressing Question 2: 
+
+library(viridis)
+plasma_pal <- c(viridis::plasma(n = 4))
+pal<-viridis(n=4)
+
+q2_plot<-ggplot(all_data,aes(jitter(YearCollected,5),psite_count))+
+  geom_point(aes(group=psite_spp,color=psite_spp),size=4,pch=19)+
+  scale_color_manual(name = c("parasite taxonomic group"), values=plasma_pal, 
+                     limits = c("trem.diplo","trem.dlum","trem.em","trem.dips"))+
+  xlab("year collected")+
+  ylab("parasite abundance (number of parasite individuals per host individual)")+
+  theme_minimal()+
+  theme(plot.title=element_text(size=18,hjust=0.5,face="plain"),
+        axis.text.y=element_text(size=14),
+        axis.title.y=element_text(size=16),
+        axis.title.x=element_text(size=16),
+        panel.background=element_rect(fill="white",color="black"),panel.grid.major=element_line(color=NA),
+        panel.grid.minor=element_line(color=NA),plot.margin=unit(c(0,0,0,0),"cm"))+
+  #annotate("text",label="effect of year:\np < 0.0001",x = 1.9, y = 0.45, size = 6)+
+  theme(legend.position="top",legend.title = element_text(size = 18),
+        legend.text = element_text(size=14))
+q2_plot
+
+
+# Addressing Question 3: 
+
+q3_plot<-ggplot(all_data,aes(jitter(Sum.of.Number.Party.Hours,10),psite_count))+
+  geom_point(aes(group=psite_spp,color=psite_spp),size=4,pch=19)+
+  scale_color_manual(name = c("parasite taxonomic group"), values=plasma_pal, 
+                     limits = c("trem.diplo","trem.dlum","trem.em","trem.dips"))+
+  xlab("year collected")+
+  ylab("parasite abundance (number of parasite individuals per host individual)")+
+  theme_minimal()+
+  theme(plot.title=element_text(size=18,hjust=0.5,face="plain"),
+        axis.text.y=element_text(size=14),
+        axis.title.y=element_text(size=16),
+        axis.title.x=element_text(size=16),
+        panel.background=element_rect(fill="white",color="black"),panel.grid.major=element_line(color=NA),
+        panel.grid.minor=element_line(color=NA),plot.margin=unit(c(0,0,0,0),"cm"))+
+  #annotate("text",label="effect of year:\np < 0.0001",x = 1.9, y = 0.45, size = 6)+
+  theme(legend.position="top",legend.title = element_text(size = 18),
+        legend.text = element_text(size=14))
+q3_plot
 
