7fdecf370a0249c4f27b35b34490d2a9832f557d
Chelsea Wood <chelsealwood@gmail.com>
2025-08-10 08:44:37 -0600

    preliminary analysis of big question

--- src/big_analysis.R
+++ src/big_analysis.R
@@ -30,30 +30,163 @@ library(dplyr)
 minus_myxos <- all_data %>%
   filter(!grepl("myx", psite_spp))
 
+
+# Add in life history strategy
+
+minus_myxos$LH_strategy<-vector("character",length(minus_myxos$CatalogNumber))
+
+for(i in 1:length(minus_myxos$CatalogNumber)) {
+  
+  if(grepl("mono", minus_myxos$psite_spp[i])){
+    minus_myxos$LH_strategy[i] <- "direct"
+    
+  } else {
+    
+    if(grepl("cope", minus_myxos$psite_spp[i])){
+      minus_myxos$LH_strategy[i] <- "direct"
+      
+    } else {
+      
+      if(grepl("crus", minus_myxos$psite_spp[i])){
+        minus_myxos$LH_strategy[i] <- "direct"
+        
+      } else {
+      
+      minus_myxos$LH_strategy[i] <- "complex"
+    }
+    }
+  }}
+
+minus_myxos$LH_strategy 
+
+thing <- minus_myxos %>%
+  group_by(LH_strategy, psite_spp) %>%
+  summarise(n = n())
+View(thing)
+
+
+# Add in parasite taxon
+
+minus_myxos$psite_taxon<-vector("character",length(minus_myxos$CatalogNumber))
+
+for(i in 1:length(minus_myxos$CatalogNumber)) {
+  
+  if(grepl("mono", minus_myxos$psite_spp[i])){
+    minus_myxos$psite_taxon[i] <- "monogene"
+    
+  } else {
+    
+    if(grepl("cope", minus_myxos$psite_spp[i])){
+      minus_myxos$psite_taxon[i] <- "copepod"
+      
+    } else {
+      
+      if(grepl("crus", minus_myxos$psite_spp[i])){
+        minus_myxos$psite_taxon[i] <- "other crustacean"
+        
+      } else {
+        
+        if(grepl("nem", minus_myxos$psite_spp[i])){
+          minus_myxos$psite_taxon[i] <- "nematode"
+          
+        } else {
+          
+          if(grepl("ces", minus_myxos$psite_spp[i])){
+            minus_myxos$psite_taxon[i] <- "cestode"
+            
+          } else {
+            
+            if(grepl("trem", minus_myxos$psite_spp[i])){
+              minus_myxos$psite_taxon[i] <- "trematode"
+              
+            } else {
+              
+              if(grepl("acan", minus_myxos$psite_spp[i])){
+                minus_myxos$psite_taxon[i] <- "acanthocephalan"
+                
+              } else {
+      
+      minus_myxos$psite_taxon[i] <- "NA"
+    }
+  }
+        }}}}}}
+
+thing <- minus_myxos %>%
+  group_by(psite_taxon, psite_spp) %>%
+  summarise(n = n())
+View(thing)
+
+
+# Messing around with models
+
 (offset(log(TotalLength_mm)))
 
-model_draft<-glmer.nb(as.numeric(psite_count)~CI*scale(YearCollected)+scale(Latitude)+scale(TotalLength_mm)+
+model_draft<-glmer.nb(as.numeric(psite_count)~CI+scale(YearCollected)+scale(Latitude)+scale(TotalLength_mm)+
                         (1|psite_spp)+(1|CatalogNumber),
                       data=minus_myxos,family="nbinom")
 summary(model_draft)
 
-big_predictions<-ggeffect(model_draft,c("YearCollected","CI"))
+
+# Parasite-taxon level
+
+model_draft<-glmer.nb(as.numeric(psite_count)~psite_taxon*scale(YearCollected)+scale(Latitude)+scale(TotalLength_mm)+
+                        (1|psite_spp)+(1|CatalogNumber),
+                      data=minus_myxos,family="nbinom")
+summary(model_draft)
+
+
+# Test the big hyps
+
+model_draft_1<-glmer.nb(as.numeric(psite_count)~CI*scale(YearCollected)+scale(Latitude)+scale(TotalLength_mm)+
+                        (1|fish_spp/psite_spp)+(1|CatalogNumber),
+                      data=minus_myxos,family="nbinom")
+summary(model_draft_1)
+
+
+
+# Three-way interaction
+
+
+model_draft_1<-glmer.nb(as.numeric(psite_count)~CI*scale(YearCollected)*LH_strategy+scale(Latitude)+scale(TotalLength_mm)+
+                          (1|fish_spp/psite_spp)+(1|CatalogNumber),
+                        data=minus_myxos,family="nbinom")
+summary(model_draft_1)
+
+
+# Then pull out the individual parasite trajectories and plot them as in the Puget Sound paper
+
+model_draft_2<-glmer.nb(as.numeric(psite_count)~CI*scale(YearCollected)*LH_strategy+scale(Latitude)+scale(TotalLength_mm)+
+                          (scale(YearCollected)|fish_spp/psite_spp)+(1|CatalogNumber),
+                        data=minus_myxos,family="nbinom")
+summary(model_draft_2)
+
+
+
+
+
+big_predictions<-ggeffect(model_draft_1,c("YearCollected","CI","LH_strategy"))
+str(big_predictions)
+
+diverging_pal <- c("#5ab4ac","#d8b365")
+darker_pal <- c("#018571","#a6611a")
 
 big_plot<-ggplot(big_predictions,aes(x,predicted),group=group,color=group)+
-  geom_point(aes(group=group,color=group),size=4,pch=19)+
-  geom_errorbar(data=big_predictions,mapping=aes(x=x,ymin=conf.low,ymax=conf.high,group=group,color=group),width=0.03)+
+  geom_ribbon(data=big_predictions,mapping=aes(x=x,ymin=conf.low,ymax=conf.high,fill=group),alpha=0.5)+
   geom_line(aes(group=group,color=group))+
-  #scale_color_manual(name = c(""),values=plasma_pal)+
+  scale_color_manual(values=darker_pal)+
+  scale_fill_manual(values=diverging_pal)+
+  facet_grid(~facet)+
   xlab("Year")+
   ylab("predicted parasite abundance\n per parasite taxon per host individual")+
   theme_minimal()+
   #labs(linetype="parasite life history strategy")+
-  theme(plot.title=element_text(size=18,hjust=0.5,face="plain"),axis.text.y=element_text(size=14),axis.title.y=element_text(size=16),
+  theme(plot.title=element_text(size=24,hjust=0.5,face="plain"),axis.text.y=element_text(size=14),
+        axis.title.y=element_text(size=16),strip.text.x = element_text(size = 20),
         axis.text.x=element_text(size=18,color="black"),axis.title.x=element_text(size=16),
         panel.background=element_rect(fill="white",color="black"),panel.grid.major=element_line(color=NA),
         panel.grid.minor=element_line(color=NA),plot.margin=unit(c(0,0,0,0),"cm"))+
-  scale_x_discrete(limits=(rev(levels(big_predictions$x))))+
-  theme(legend.position="top",legend.title = element_text(size = 18),
-        legend.text = element_text(size=12))
+  #scale_x_discrete(limits=(rev(levels(big_predictions$x))))+
+  theme(legend.position="top",legend.title = element_blank(),
+        legend.text = element_text(size=20))
 big_plot
 
